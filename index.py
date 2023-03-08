c98b1d2cdd9be77659bde4a77f5c7f45e4cffbbd
unknown <mathieu.pouliot@outlook.com>
2023-03-08 13:20:21 -0500

    updated file

--- index.py
+++ index.py
@@ -5,4 +5,17 @@ sp_500_tickers = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_c
 sp_500_tickers = sp_500_tickers[0]
 ticker_list = sp_500_tickers["Symbol"].values.tolist()
 
-print(ticker_list)
+
+#print(ticker_list)
+
+import datetime as dt 
+import pandas as pd
+
+import concurrent.futures as cf
+from yahoofinancials import YahooFinancials
+
+import re
+import ast
+import time
+import requests
+from bs4 import BeautifulSoup
