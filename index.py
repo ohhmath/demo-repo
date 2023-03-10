cef8e7a7d34e6b2356d3a4f288c951720ed16397
unknown <mathieu.pouliot@outlook.com>
2023-03-10 11:15:45 -0500

    modif

--- index.py
+++ index.py
@@ -1,21 +1,19 @@
-
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 
-sp_500_tickers = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
-sp_500_tickers = sp_500_tickers[0]
-ticker_list = sp_500_tickers["Symbol"].values.tolist()
+
+raw_csv_data = pd.read_csv(r'C:\Users\Mathieu\Documents\Python\GSPTSE.csv')
+df_comp = raw_csv_data.copy()
+
+df_comp.Close.plot(figsize=(15,4),title= "Low graph")
 
 
-#print(ticker_list)
+df_comp.Close.plot(figsize=(10,5),title= "Low graph")
+df_comp.Open.plot(figsize=(10,5),title= "Low graph")
+plt.title("Open vs Close")
+plt.show()
 
-import datetime as dt 
-import pandas as pd
-
-import concurrent.futures as cf
-from yahoofinancials import YahooFinancials
-
-for j in range(3):
-	for i in range (5):
-		print("Hello world")
-
- 
+pd.to_datetime(df_comp.Date)
